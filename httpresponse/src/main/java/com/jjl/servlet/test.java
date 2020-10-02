f1b21d8fc39f5c9f1ff0d6fb43dacb67732faaac
aibianchengdejjle <1044839685@qq.com>
2020-10-02 16:52:33 +0800

    add

--- httpresponse/src/main/java/com/jjl/servlet/test.java
+++ httpresponse/src/main/java/com/jjl/servlet/test.java
@@ -1,4 +1,19 @@
 package com.jjl.servlet;
 
-public class test {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class test extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // System.out.println("这里进入请求了");
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        System.out.println(username+"   "+password);
+        resp.sendRedirect("/httpresponse_war/success.jsp");
+
+    }
 }
